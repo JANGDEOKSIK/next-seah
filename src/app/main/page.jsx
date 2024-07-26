90f512ed9a38efe39cbd47af9fd5ba397268489b
HANSEONGJU-ez <63626322+HANSEONGJU-ez@users.noreply.github.com>
2024-07-26 17:12:31 +0900

    input component 제작

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,15 +1,18 @@
 import { useSelector } from "react-redux";
 import PageTransition from "../_components/PageTransition"
+import Input from "../_components/Input";
 
-export default async function MainPage() {
-  const promise = await fetch(`${process.env.REACT_APP_BASE_URL}/dummy/menu.json`);
-  const response = await promise.json();
+export default function MainPage() {
+  // const promise = await fetch(`${process.env.REACT_APP_BASE_URL}/dummy/menu.json`);
+  // const response = await promise.json();
 
-  console.log(response);
+  // console.log(response);
   return (
     <PageTransition>
       <div>
         234i234782348237949
+        <Input id="myTit" isEssential={true} isVertical={true} label="제목" placeholder="제목을 입력해 주세요." />
+        <Input id="myNumber" isEssential={true} isVertical={false} label="번호" placeholder="번호를 입력해 주세요." />
       </div>
     </PageTransition>
   )
