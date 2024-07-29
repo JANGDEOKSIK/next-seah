7fce30d01ae3d870526692c645a1ad812cecf1ef
KANGMINJI <kminji@easymedia.net>
2024-07-29 10:38:07 +0900

    redux-persist오류 뜨는 것 해결

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,28 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import storage from "redux-persist/lib/storage";
 import testSlice from "./slices/testSlice";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
 
 const reducers = combineReducers({
   testSlice
 })
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  }
+}
+
+const storage = typeof window === "undefined" ? createNoopStorage() : createWebStorage("local");
 
 const persistConfig = {
   key: "root",
