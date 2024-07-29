2f0ce8477b41018d471bc6d6b474f18445d570eb
HANSEONGJU-ez <63626322+HANSEONGJU-ez@users.noreply.github.com>
2024-07-29 17:19:10 +0900

    selectbox 작업

---
+++ src/app/_components/SelectBox.jsx
@@ -0,0 +1,39 @@
+export default function SelectBox({id, label, isEssential, option0, option1}){
+  return(
+    <>
+      <div className="input-wrap">
+        <div className="input-tit">
+          <div className="label">
+            <label htmlFor={id}>{label}</label>
+            {isEssential && <span>*</span>}
+          </div>
+        </div>
+        <div className="form-select-arr">
+          <div className="form-select">
+            <select>
+              {
+                option0.map((item) => {
+                  return(
+                    <option value={item.value} key={item.value}>{item.name}</option>
+                  )
+                })
+              }
+            </select>
+          </div>
+          <div className="form-select">
+            <select>
+              {
+                option1.map((item) => {
+                  return(
+                    <option value={item.value} key={item.value}>{item.name}</option>
+                  )
+                })
+              }
+            </select>
+          </div>
+        </div>
+      </div>
+      
+    </>
+  )
+}
--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -5,6 +5,7 @@ import PageTransition from "../_components/layout/PageTransition";
 import { useState } from "react";
 import Input from "../_components/Input";
 import Textarea from "../_components/Textarea";
+import SelectBox from "../_components/SelectBox";
 
 export default function MainPage() {
   // const promise = await fetch(`${process.env.REACT_APP_BASE_URL}/dummy/menu.json`);
@@ -15,6 +16,17 @@ export default function MainPage() {
   const [inputTitValue, setInputTitValue] = useState("");
   const [inputNumValue, setInputNumValue] = useState("");
   const [textareaValue, setTextareaValue] = useState("");
+  const OPTIONS0 = [
+    { value: "세아홀딩스 계열", name: "세아홀딩스 계열" },
+    { value: "세아홀딩스 계열1", name: "세아홀딩스 계열1" },
+    { value: "세아홀딩스 계열2", name: "세아홀딩스 계열2" },
+  ];
+
+  const OPTIONS1 = [
+    { value: "세아제강지주 계열", name: "세아제강지주 계열" },
+    { value: "세아제강지주 계열1", name: "세아제강지주 계열1" },
+    { value: "세아제강지주 계열2", name: "세아제강지주 계열2" },
+  ];
 
   const onClick = () => {
     console.log(inputTitValue);
@@ -32,6 +44,7 @@ export default function MainPage() {
         <Input id="myTit" isEssential={true} label="제목" placeholder="제목을 입력해 주세요." value={setInputTitValue} />
         <Input id="myNumber" isEssential={true} isVertical={true} label="번호" placeholder="번호를 입력해 주세요." value={setInputNumValue} />
         <Textarea id="content" isNoTitle={true} isEssential={true} label="내용" placeholder="내용을 입력해 주세요." value={setTextareaValue} textareaValue={textareaValue} />
+        <SelectBox id="company" isEssential={true} label="대상회사" option0={OPTIONS0} option1={OPTIONS1} />
         <button onClick={onClick}>제출하기</button>
       </div>
     </PageTransition>
