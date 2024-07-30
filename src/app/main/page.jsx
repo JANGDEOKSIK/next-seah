7a5199a34d3477b0f5c6c5a84a990cd4b71fd676
HANSEONGJU-ez <63626322+HANSEONGJU-ez@users.noreply.github.com>
2024-07-30 10:12:23 +0900

    hsj 데이터 삭제

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -41,11 +41,6 @@ export default function MainPage() {
     <PageTransition>
       <div>
         234i234782348237949
-        <Input id="myTit" isEssential={true} label="제목" placeholder="제목을 입력해 주세요." value={setInputTitValue} />
-        <Input id="myNumber" isEssential={true} isVertical={true} label="번호" placeholder="번호를 입력해 주세요." value={setInputNumValue} />
-        <Textarea id="content" isNoTitle={true} isEssential={true} label="내용" placeholder="내용을 입력해 주세요." value={setTextareaValue} textareaValue={textareaValue} />
-        <SelectBox id="company" isEssential={true} label="대상회사" option0={OPTIONS0} option1={OPTIONS1} />
-        <button onClick={onClick}>제출하기</button>
       </div>
     </PageTransition>
   );
