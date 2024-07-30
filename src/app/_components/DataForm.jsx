f299a24180a17e452df35cb9a7cc6fcdeb3b6b64
EunJiKU8151 <yoo128@easymedia.net>
2024-07-30 09:25:41 +0900

    은지꺼 브랜치로 이동

--- src/app/_components/DataForm.jsx
+++
@@ -1,9 +0,0 @@
-const DataForm = ({ children }) => {
-  return (
-    <div className="data-form">
-      { children }
-    </div>
-  )
-}
-
-export default DataForm;
--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -3,9 +3,6 @@
 import { useSelector } from "react-redux";
 import PageTransition from "../_components/layout/PageTransition";
 import { Children, useState } from "react";
-import DataForm from "@/app/_components/DataForm";
-import Row from "@/app/_components/Row";
-import { Input2, Select2, TextArea2 } from "@/app/_components/Input2";
 import Input from "../_components/Input";
 import Textarea from "../_components/Textarea";
 import SelectBox from "../_components/SelectBox";
@@ -36,31 +33,6 @@ export default function MainPage() {
     setTextareaValue(e.target.value.length);
   };
 
-  // 😇은지 작업중😇
-  // Dummy
-  const selectDummy = [
-    {
-      optionIdx : 1,
-      tit: "구은지",
-      option: ["삼겹살", "김치", "초코렛"]
-    },
-    {
-      optionIdx : 2,
-      tit: "나르",
-      option: ["츄르", "사료", "마띠마띠"]
-    },
-    {
-      optionIdx : 3,
-      tit: "토똥이",
-      option: ["티모시", "딸기", "영양제"]
-    },
-  ]
-
-  const {register, handleSubmit, formState: {errors}} = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-  }
-  // 😇은지 작업중😇
   const test = (e) => {};
 
   return (
@@ -99,32 +71,6 @@ export default function MainPage() {
           option1={OPTIONS1}
         />
         <button onClick={onClick}>제출하기</button>
-
-        <hr style={{marginTop: 30, marginBottom: 30}}/>
-
-        {/* 😇은지 작업중😇 */}
-        <DataForm>
-          <Row thTit={"대상회사"} isRequired={true}>
-            <div className="form-group">
-              <Select2 selectData={selectDummy} />
-              <Select2 selectData={selectDummy}  />
-            </div>
-          </Row>
-          <Row thTit={"제보대상"} isRequired={false} inputId={"input1"}>
-            <div className="form-group">
-              <Input2 placeholder={"이름을 입력 해주세요."} />
-            </div>
-          </Row>
-          <Row thTit={"내용"} isRequired={true}>
-            <TextArea2 placeholder={"내용을 입력 해주세요."} maxByte={4000} />
-          </Row>
-        </DataForm>
-        <form onSubmit={onSubmit}>
-          <input type="text" id="eee" {...register("eee", {required: "값을 넣어라 닝겐!"})} />
-          <button type="submit">submit</button>
-        </form>
-        <button className="btn2" type="button">제출하기</button>
-        {/* 😇은지 작업중😇 */}
       </div>
     </PageTransition>
   );
