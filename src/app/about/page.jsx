ef9253b0e53b03be4c552b81984b729722338e03
YEOJISEUL <zzzseulxx@easymedia.net>
2024-07-30 12:06:01 +0900

    props test2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,8 @@ export default function AboutPage() {
 	return (
 		<div>
 			<h1>지슬</h1>
-			<NewInput isEss={true} isLabel={true} label={"메롱"} placeholder={"작성"}/>
+			<NewInput isLabel={true} label={"짜잔"} isEss={true} placeholder={"작성"}/>
+			<NewInput isLabel={true} label={"짜자잔"} isEss={false} placeholder={"쓰세요"}/>
 		</div>
 	)
 }
