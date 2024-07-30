5b30f8b6e521ae0ea238a2963c75824d6ffc3640
HANSEONGJU-ez <63626322+HANSEONGJU-ez@users.noreply.github.com>
2024-07-30 10:15:56 +0900

    hsj 데이터 삭제

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -3,38 +3,9 @@
 import { useSelector } from "react-redux";
 import PageTransition from "../_components/layout/PageTransition";
 import { Children, useState } from "react";
-import Input from "../_components/Input";
-import Textarea from "../_components/Textarea";
-import SelectBox from "../_components/SelectBox";
 import { useForm } from "react-hook-form";
 
 export default function MainPage() {
-  const [inputTitValue, setInputTitValue] = useState("");
-  const [inputNumValue, setInputNumValue] = useState("");
-  const [textareaValue, setTextareaValue] = useState("");
-  const OPTIONS0 = [
-    { value: "세아홀딩스 계열", name: "세아홀딩스 계열" },
-    { value: "세아홀딩스 계열1", name: "세아홀딩스 계열1" },
-    { value: "세아홀딩스 계열2", name: "세아홀딩스 계열2" },
-  ];
-
-  const OPTIONS1 = [
-    { value: "세아제강지주 계열", name: "세아제강지주 계열" },
-    { value: "세아제강지주 계열1", name: "세아제강지주 계열1" },
-    { value: "세아제강지주 계열2", name: "세아제강지주 계열2" },
-  ];
-
-  const onClick = () => {
-    console.log(inputTitValue);
-    console.log(inputNumValue);
-  };
-
-  const onTextAreaHandler = (e) => {
-    setTextareaValue(e.target.value.length);
-  };
-
-  const test = (e) => {};
-
   return (
     <PageTransition>
       <div>
