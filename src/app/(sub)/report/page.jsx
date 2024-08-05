5edaaf5489d2c5ce261cd7985e3c82131f8eb35a
YEOJISEUL <zzzseulxx@easymedia.net>
2024-08-05 14:06:01 +0900

    세팅

---
+++ src/app/(sub)/report/page.jsx
@@ -0,0 +1,23 @@
+
+"use client";
+
+export default function GuidePage() {
+	return (
+		<div>
+			<header>
+				<a href="" className="logo"><img src="/images/logo-wht.png" alt="" /></a>
+				<ul>
+					<li><a href="" className="one-dep">윤리경영제안센터 소개</a></li>
+					<li><a href="" className="one-dep">제보하기</a>
+						<div>
+							<a href="" className="two-dep">제보 가이드</a>
+							<a href="" className="two-dep">온라인 제보</a>
+							<a href="" className="two-dep">제보결과 확인</a>
+							<a href="" className="two-dep">서신·전화·Fax 이용안내</a>
+						</div>
+					</li>
+				</ul>
+			</header>
+		</div>
+	)
+}
