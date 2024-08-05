2baa59a1c2373f7c3f0213f5b93cf285f90d1ef2
YEOJISEUL <zzzseulxx@easymedia.net>
2024-08-05 18:01:45 +0900

    헤더 작업 중

--- src/app/(sub)/report/page.jsx
+++ src/app/(sub)/report/page.jsx
@@ -7,17 +7,45 @@ export default function GuidePage() {
 			<header>
 				<a href="" className="logo"><img src="/images/logo-wht.png" alt="" /></a>
 				<ul>
-					<li><a href="" className="one-dep">윤리경영제안센터 소개</a></li>
-					<li><a href="" className="one-dep">제보하기</a>
+					<li>
+						<a href="" className="one-dep f-body1-eb"><span>윤리경영제안센터 소개</span></a>
+					</li>
+					<li>
+						<a href="" className="one-dep f-body1-eb"><span>제보하기</span></a>
 						<div>
-							<a href="" className="two-dep">제보 가이드</a>
-							<a href="" className="two-dep">온라인 제보</a>
-							<a href="" className="two-dep">제보결과 확인</a>
-							<a href="" className="two-dep">서신·전화·Fax 이용안내</a>
+							<a href="" className="two-dep f-body2-b"><span>제보 가이드</span></a>
+							<a href="" className="two-dep f-body2-b"><span>온라인 제보</span></a>
+							<a href="" className="two-dep f-body2-b"><span>제보결과 확인</span></a>
+							<a href="" className="two-dep f-body2-b"><span>서신·전화·Fax 이용안내</span></a>
 						</div>
 					</li>
 				</ul>
+				<div className="utill">
+					<div className="lang">
+						<a href="" className="on">KOR</a>
+						<a href="" className="on">ENG</a>
+					</div>
+					<a href="" className="all-menu"></a>
+				</div>
 			</header>
+
+			<div className="wrap">
+
+				<div className="sub-vis" style={{background: "url(/images/sub-vis1.jpg)"}}>
+					<p>제보 가이드</p>
+					<div className="lnb">
+						<a href="" className="on">제보 가이드</a>
+						<a href="">온라인 제보</a>
+						<a href="">제보결과 확인</a>
+						<a href="">서신·전화·Fax 이용안내</a>
+					</div>
+				</div>
+
+				<div className="content">
+					
+				</div>
+
+			</div>
 		</div>
 	)
 }
