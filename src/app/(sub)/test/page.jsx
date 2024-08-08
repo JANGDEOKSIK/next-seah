ee81ef1c4859154e54a8e5eebdcf936123d6302f
Seulgi-Choi <csg1334@gmail.com>
2024-08-08 17:35:28 +0900

    fetch.js staleTime 주석

---
+++ src/app/(sub)/test/page.jsx
@@ -0,0 +1,10 @@
+import PrefetchElement from "@/app/_lib/prefetch";
+import Header from "@/app/_components/layout/Header";
+
+export default function TestPage() {
+  return (
+    <>
+      <h3>header 테스트</h3>
+    </>
+  );
+}
