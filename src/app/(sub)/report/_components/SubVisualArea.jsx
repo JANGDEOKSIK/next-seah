4306630afafa90a5bea95dd17bb47b27a68e8213
SEOYOONJEONG-ez <112161928+yoonjeongseo@users.noreply.github.com>
2024-08-20 11:44:11 +0900

    서브 비주얼 공통 reset

--- src/app/(sub)/report/_components/SubVisualArea.jsx
+++ src/app/(sub)/report/_components/SubVisualArea.jsx
@@ -1,31 +1,17 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 
 export default function SubVisualArea() {
-  const datas = require("/db.json"); // db.json에서 데이터 가져오기
   const pathname = usePathname();
-
-  console.log(pathname); // url 확인
-  console.log(datas);
-
-  // url이 pathname과 일치하는 항목 필터링
-  const matchedItem = datas.menu.find((item) => item.url === pathname);
+  console.log(pathname);
 
   return (
     <>
       {pathname.indexOf("finish") < 0 ? (
-        <div className="subvisual">
-          {matchedItem ? (
-            <div className="subvisual">
-              <div>
-                <h2 className="f-sub1 fc-white">{matchedItem.name}</h2>
-              </div>
-            </div>
-          ) : null}
-        </div>
+        <div className="subvisual"></div>
       ) : (
-        <>finish</>
+        <></>
       )}
     </>
   );
