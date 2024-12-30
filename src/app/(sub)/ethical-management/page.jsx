ef6a1d8893428c12c1dfd02ead78690348a1977b
이지혜 <89819830+jihye1ee@users.noreply.github.com>
2024-12-30 18:05:30 +0900

    윤리경영제안센터 소개 페이지 작업 중

--- src/app/(sub)/ethical-management/page.jsx
+++ src/app/(sub)/ethical-management/page.jsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function ethicalManagementPage() {
-  const [isActive, setIsActive] = useState(false);
   gsap.registerPlugin(ScrollTrigger, useGSAP);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsActive(true);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   useGSAP(() => {
+    const $kvTitle = document.querySelector(".visual-area.ethical h2");
+    const $kvDesc = document.querySelector(".visual-area.ethical .desc");
+
+    gsap.to(".visual-area.ethical", {
+      scrollTrigger: {
+        trigger: ".visual-area.ethical",
+        start: "top top",
+        end: "+=200%",
+        pin: true,
+        onUpdate: (self) => {
+          let percentage = (self.progress * 100).toFixed(2);
+
+          if (percentage >= 50) {
+            $kvTitle.classList.add("hidden");
+            $kvDesc.classList.add("active");
+          } else {
+            $kvTitle.classList.remove("hidden");
+            $kvDesc.classList.remove("active");
+          }
+        },
+      },
+    });
+
     gsap.to(".tip-off-wrap .left", {
       scrollTrigger: {
         trigger: ".tip-off-wrap .left",
@@ -31,9 +44,7 @@ export default function ethicalManagementPage() {
   return (
     <>
       <div className="visual-area ethical">
-        <h2 className={`f-exBold ${isActive ? "active" : ""}`}>
-          윤리경영제안센터 소개
-        </h2>
+        <h2 className="f-exBold">윤리경영제안센터 소개</h2>
         <p className="f-exBold desc">
           세아는 기본과 원칙을 지키는 윤리경영을 통해 <br />
           기업의 사회적 책임을 다하고, 풍부한 가치를 창조함으로써 고객, 임직원,
