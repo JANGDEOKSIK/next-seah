46dec77a7229363e1f0aacb074a11716296781ce
이지혜 <89819830+jihye1ee@users.noreply.github.com>
2025-01-08 13:54:14 +0900

    윤리경영제안센터 소개 페이지 작업 중

--- src/app/(sub)/ethical-management/page.jsx
+++ src/app/(sub)/ethical-management/page.jsx
@@ -11,11 +11,13 @@ export default function ethicalManagementPage() {
     const $kvTitle = document.querySelector(".top-wrap h2");
     const $kvDesc = document.querySelector(".top-wrap .desc");
 
-    gsap.fromTo(
-      $kvTitle,
-      { y: 70, opacity: 0 },
-      { y: 0, opacity: 1, duration: 0.7 }
-    );
+    setTimeout(() => {
+      gsap.fromTo(
+        $kvTitle,
+        { y: 70, opacity: 0 },
+        { y: 0, opacity: 1, duration: 0.7 }
+      );
+    }, 500);
 
     gsap.to(".top-wrap", {
       scrollTrigger: {
@@ -53,53 +55,55 @@ export default function ethicalManagementPage() {
   return (
     <>
       <div className="contents-area ethical">
-        <div className="top-wrap">
-          <h2 className="f-exBold">윤리경영제안센터 소개</h2>
-          <p className="f-exBold desc">
-            세아는 기본과 원칙을 지키는 윤리경영을 통해 <br />
-            기업의 사회적 책임을 다하고, 풍부한 가치를 창조함으로써 고객,
-            임직원,
-            <br />
-            협력회사, 주주가 함께 성장, 발전하는데 최선을 다하고 있습니다.
-          </p>
-        </div>
-
-        <div className="protection-wrap">
-          <div className="txt-box">
-            <p className="f-exBold">제보자 보호</p>
-            <div className="txt-list">
-              <div className="txt-item">
-                <p className="tit f-exBold">Anonymous</p>
-                <p className="desc f-bold">제안은 익명으로도 가능합니다</p>
-              </div>
-              <div className="txt-item">
-                <p className="tit f-exBold">Secret</p>
-                <p className="desc f-bold">
-                  제안자의 비밀은 절대적으로 보호됩니다.
-                </p>
-              </div>
-              <div className="txt-item">
-                <p className="tit f-exBold">Security</p>
-                <p className="desc f-bold">
-                  제안 내용은 세아홀딩스 감사실만이 열람가능합니다.
-                </p>
-              </div>
-            </div>
+        <div className="fixed-wrap">
+          <div className="top-wrap">
+            <h2 className="f-exBold">윤리경영제안센터 소개</h2>
+            <p className="f-exBold desc">
+              세아는 기본과 원칙을 지키는 윤리경영을 통해 <br />
+              기업의 사회적 책임을 다하고, 풍부한 가치를 창조함으로써 고객,
+              임직원,
+              <br />
+              협력회사, 주주가 함께 성장, 발전하는데 최선을 다하고 있습니다.
+            </p>
           </div>
 
-          <div className="bg-box active">
-            <img
-              src="/images/img-annoymous-bg.jpg"
-              alt=""
-            />
-            <img
-              src="/images/img-secret-bg.jpg"
-              alt=""
-            />
-            <img
-              src="/images/img-security-bg.jpg"
-              alt=""
-            />
+          <div className="protection-wrap">
+            <div className="txt-box">
+              <p className="f-exBold">제보자 보호</p>
+              <div className="txt-list">
+                <div className="txt-item">
+                  <p className="tit f-exBold">Anonymous</p>
+                  <p className="desc f-bold">제안은 익명으로도 가능합니다</p>
+                </div>
+                <div className="txt-item">
+                  <p className="tit f-exBold">Secret</p>
+                  <p className="desc f-bold">
+                    제안자의 비밀은 절대적으로 보호됩니다.
+                  </p>
+                </div>
+                <div className="txt-item">
+                  <p className="tit f-exBold">Security</p>
+                  <p className="desc f-bold">
+                    제안 내용은 세아홀딩스 감사실만이 열람가능합니다.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-box active">
+              <img
+                src="/images/img-annoymous-bg.jpg"
+                alt=""
+              />
+              <img
+                src="/images/img-secret-bg.jpg"
+                alt=""
+              />
+              <img
+                src="/images/img-security-bg.jpg"
+                alt=""
+              />
+            </div>
           </div>
         </div>
 
